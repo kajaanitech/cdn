8c94c732415276cb7e947d6d38c73ee043ea50e2
mazkdevf <79049205+mazkdevf@users.noreply.github.com>
2023-02-02 19:59:14 +0200

    Remove pricing

--- assets/js/main.js
+++ assets/js/main.js
@@ -37,27 +37,6 @@ $(document).ready(async function () {
                         type = `Alkaen`;
                     }
 
-                    const $targetEl = document.getElementById(`${element?.id}-description`);
-
-                    const $triggerEl = document.getElementById(`${element?.id}-button`);
-
-                    const options = {
-                        placement: 'bottom',
-                        triggerType: 'hover',
-                        offset: 10,
-                        onHide: () => {
-                            console.log('popover is shown');
-                        },
-                        onShow: () => {
-                            console.log('popover is hidden');
-                        },
-                        onToggle: () => {
-                            console.log('popover is toggled');
-                        }
-                    };
-
-                    const popover = new Popover($targetEl, $triggerEl, options);
-
                     temp += `
                     
                     <div class="w-full max-w-sm bg-white rounded-lg shadow-md">
@@ -70,21 +49,11 @@ $(document).ready(async function () {
                             </h5>
                             <div class="flex items-center mt-2.5 mb-5"></div>
                             <div class="justify-between items-center">
-                                <span class="text-3xl font-bold text-gray-900">${element?.hinta}€</span> ${type} <button id="${element?.id}-button" data-popover-target="${element?.id}-description" data-popover-placement="bottom-end" type="button"><svg class="w-4 h-4 ml-2 text-gray-400 hover:text-gray-500" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd"></path></svg><span class="sr-only">Show information</span></button>
+                                <span class="text-3xl font-bold text-gray-900">${element?.hinta}€</span> ${type}
                             </div>
                             ${desc}
                         </div>
-                    </div>
-                    
-                    <div data-popover id="${element?.id}-description" role="tooltip" class="absolute z-10 invisible inline-block text-sm font-light text-gray-500 transition-opacity duration-300 bg-white border border-gray-200 rounded-lg shadow-sm opacity-0 w-72">
-                        <div class="p-3 space-y-2">
-                            <h3 class="font-semibold text-gray-900 dark:text-white">${element?.nimi} - Hinnoittelu</h3>
-                            <p>${element?.lisatieto}</p>
-                        </div>
-                        <div data-popper-arrow></div>
-                    </div>
-
-                    `;
+                    </div>`;
                 }
             });
 

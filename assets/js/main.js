a6ea9f56a947337f805bd0e381c28cd9deffaa40
mazkdevf <79049205+mazkdevf@users.noreply.github.com>
2023-02-02 17:57:55 +0200

    Upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -40,42 +40,42 @@ $(document).ready(async function () {
                     temp += `
                     
                     <div class="w-full max-w-sm bg-white rounded-lg shadow-md">
-            <div class="grid place-items-center my-6">
-                <img src="${element?.kuva}" class="p-12 w-32 h-32 rounded-lg" alt="Palvelun Kuva">
-            </div>
-            <div class="px-5 pb-5 my-2">
-                <h5 class="text-xl font-semibold tracking-tight text-gray-900">
-                    ${element?.nimi}
-                </h5>
-                <div class="flex items-center mt-2.5 mb-5"></div>
-                <div class="justify-between items-center">
-                    <span class="text-3xl font-bold text-gray-900">${element?.hinta}€</span> ${type}
-                </div>
-                ${desc}
-            </div>
-            <div class="cursor-pointer text-sm mt-4 font-semibold tracking-tight text-gray-900" data-modal-target="${element?.id}-modal" data-modal-toggle="${element?.id}-modal">Hinnan hinnoittelu</div>
-        </div>
+                        <div class="grid place-items-center my-6">
+                            <img src="${element?.kuva}" class="p-12 w-32 h-32 rounded-lg" alt="Palvelun Kuva">
+                        </div>
+                        <div class="px-5 pb-5 my-2">
+                            <h5 class="text-xl font-semibold tracking-tight text-gray-900">
+                                ${element?.nimi}
+                            </h5>
+                            <div class="flex items-center mt-2.5 mb-5"></div>
+                            <div class="justify-between items-center">
+                                <span class="text-3xl font-bold text-gray-900">${element?.hinta}€</span> ${type}
+                            </div>
+                            ${desc}
+                        </div>
+                        <div class="cursor-pointer text-sm mt-4 font-semibold tracking-tight text-gray-900" data-modal-target="${element?.id}-modal" data-modal-toggle="${element?.id}-modal" type="button">Hinnan hinnoittelu</div>
+                    </div>
         
-        <div id="${element?.id}-modal" tabindex="-1" aria-hidden="true" class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-modal md:h-full">
-        <div class="relative w-full h-full max-w-md md:h-auto">
-            <div class="relative bg-white rounded-lg shadow">
-                <div class="flex items-start justify-between p-4 border-b rounded-t">
-                    <h3 class="text-xl font-semibold text-gray-900">
-                        ${element?.nimi} - Hinnoittelu
-                    </h3>
-                    <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center" data-modal-hide="${element?.id}-modal">
-                        <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-                        <span class="sr-only">Sulje modaali</span>
-                    </button>
-                </div>
-                <div class="p-6 space-y-6">
-                    <p class="text-base leading-relaxed text-gray-500">
-                        ${element?.lisatieto}
-                    </p>
-                </div>
-            </div>
-        </div>
-        </div>`;
+                    <div id="${element?.id}-modal" tabindex="-1" aria-hidden="true" class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-modal md:h-full">
+                        <div class="relative w-full h-full max-w-md md:h-auto">
+                            <div class="relative bg-white rounded-lg shadow">
+                                <div class="flex items-start justify-between p-4 border-b rounded-t">
+                                    <h3 class="text-xl font-semibold text-gray-900">
+                                        ${element?.nimi} - Hinnoittelu
+                                    </h3>
+                                    <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center" data-modal-hide="${element?.id}-modal">
+                                        <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                                        <span class="sr-only">Sulje modaali</span>
+                                    </button>
+                                </div>
+                                <div class="p-6 space-y-6">
+                                    <p class="text-base leading-relaxed text-gray-500">
+                                        ${element?.lisatieto}
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>`;
                 }
             });
 

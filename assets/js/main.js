9f813e0e88185a13f1020b773a30419460bd7bc1
mazkdevf <79049205+mazkdevf@users.noreply.github.com>
2023-02-02 19:52:51 +0200

    update test

--- assets/js/main.js
+++ assets/js/main.js
@@ -49,33 +49,21 @@ $(document).ready(async function () {
                             </h5>
                             <div class="flex items-center mt-2.5 mb-5"></div>
                             <div class="justify-between items-center">
-                                <span class="text-3xl font-bold text-gray-900">${element?.hinta}€</span> ${type}
+                                <span class="text-3xl font-bold text-gray-900">${element?.hinta}€</span> ${type} <button data-popover-target="${element?.id}-description" data-popover-placement="bottom-end" type="button"><svg class="w-4 h-4 ml-2 text-gray-400 hover:text-gray-500" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd"></path></svg><span class="sr-only">Show information</span></button>
                             </div>
                             ${desc}
                         </div>
-                        <div class="cursor-pointer text-sm mt-4 font-semibold tracking-tight text-gray-900" data-modal-target="${element?.id}-modal" data-modal-toggle="${element?.id}-modal" type="button">Hinnan hinnoittelu</div>
                     </div>
-        
-                    <div id="${element?.id}-modal" tabindex="-1" aria-hidden="true" class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-modal md:h-full">
-                        <div class="relative w-full h-full max-w-md md:h-auto">
-                            <div class="relative bg-white rounded-lg shadow">
-                                <div class="flex items-start justify-between p-4 border-b rounded-t">
-                                    <h3 class="text-xl font-semibold text-gray-900">
-                                        ${element?.nimi} - Hinnoittelu
-                                    </h3>
-                                    <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center" data-modal-hide="${element?.id}-modal">
-                                        <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-                                        <span class="sr-only">Sulje modaali</span>
-                                    </button>
-                                </div>
-                                <div class="p-6 space-y-6">
-                                    <p class="text-base leading-relaxed text-gray-500">
-                                        ${element?.lisatieto}
-                                    </p>
-                                </div>
-                            </div>
+                    
+                    <div data-popover id="${element?.id}-description" role="tooltip" class="absolute z-10 invisible inline-block text-sm font-light text-gray-500 transition-opacity duration-300 bg-white border border-gray-200 rounded-lg shadow-sm opacity-0 w-72">
+                        <div class="p-3 space-y-2">
+                            <h3 class="font-semibold text-gray-900 dark:text-white">${element?.nimi} - Hinnoittelu</h3>
+                            <p>${element?.lisatieto}</p>
                         </div>
-                    </div>`;
+                        <div data-popper-arrow></div>
+                    </div>
+
+                    `;
                 }
             });
 

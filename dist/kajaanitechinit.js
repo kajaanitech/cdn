8bbddacb70e1621c67d7d30d42e825b27abcf194
mazkdevf (Make) <79049205+mazkdevf@users.noreply.github.com>
2024-03-19 19:01:33 +0200

    Add kajaanitechinit.js

---
+++ dist/kajaanitechinit.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', function () {
+    function kti() {
+        console.clear();
+        console.log(`
+    
+    
+██╗  ██╗ █████╗      ██╗ █████╗  █████╗ ███╗   ██╗██╗████████╗███████╗ ██████╗██╗  ██╗   ███████╗██╗
+██║ ██╔╝██╔══██╗     ██║██╔══██╗██╔══██╗████╗  ██║██║╚══██╔══╝██╔════╝██╔════╝██║  ██║   ██╔════╝██║
+█████╔╝ ███████║     ██║███████║███████║██╔██╗ ██║██║   ██║   █████╗  ██║     ███████║   █████╗  ██║
+██╔═██╗ ██╔══██║██   ██║██╔══██║██╔══██║██║╚██╗██║██║   ██║   ██╔══╝  ██║     ██╔══██║   ██╔══╝  ██║
+██║  ██╗██║  ██║╚█████╔╝██║  ██║██║  ██║██║ ╚████║██║   ██║   ███████╗╚██████╗██║  ██║██╗██║     ██║
+╚═╝  ╚═╝╚═╝  ╚═╝ ╚════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝╚═╝   ╚═╝   ╚══════╝ ╚═════╝╚═╝  ╚═╝╚═╝╚═╝     ╚═╝
+
+
+[INFO]: Moikka, tänne on rakentumassa uusi verkkosivusto!
+[INFO]: Mikäli juuri sinä olet kiinnostunut verkkosivuista tai muista IT / ATK Alan palveluista menethän osoitteeseen : https://www.kajaanitech.fi/
+    `)
+    }
+
+    console.clear();
+    setInterval(kti, 3000);
+});
